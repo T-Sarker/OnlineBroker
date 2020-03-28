18a5729bc0613f2aa49d75a37cd6edc34043aba2
T-Sarker <tapos.k.sarker25@gmail.com>
2020-03-29 01:33:10 +0600

    Broker- khansama extended admin inc seperation

---
+++ admin/inc/sidebar.php
@@ -0,0 +1,95 @@
+<div class="sidebar clearfix">
+        <ul class="sidebar-panel nav metismenu" id="side-menu" data-plugin="metismenu">
+            <li class="active"><a href="#"><span class="icon color5"><i class="fa fa-home" aria-hidden="true"></i></span><span class="nav-title">Dashboard</span> <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="index.html" class="active"><i class="fa fa-angle-right" aria-hidden="true"></i> Dashboard 1</a></li>
+                    <li><a href="index-2.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Dashboard 2</a></li>
+                    <li><a href="index-3.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Dashboard 3</a></li>
+                </ul>
+            </li>
+            <li class="sidetitle">FEATURES</li>
+            <li><a href="#"><span class="icon color7"><i class="fa fa-flask" aria-hidden="true"></i></span>UI Elements <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="icons.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Icons</a></li>
+                    <li><a href="tabs.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Tabs</a></li>
+                    <li><a href="buttons.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Buttons</a></li>
+                    <li><a href="loading-buttons.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Loading Buttons</a></li>
+                    <li><a href="carousel.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Carousel</a></li>
+                    <li><a href="timeline.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Timeline</a></li>
+                    <li><a href="panels.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Panels</a></li>
+                    <li><a href="notifications.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Notifications</a></li>
+                    <li><a href="modal-boxes.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Modal Boxes</a></li>
+                    <li><a href="progress-bars.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Progress Bars</a></li>
+                    <li><a href="page-header.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Page Header</a></li>
+                    <li><a href="menu-style.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Menu Style</a></li>
+                    <li><a href="others.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Others<span class="label label-danger">3</span></a></li>
+                </ul>
+            </li>
+            <li><a href="#"><span class="icon color9"><i class="fa fa-line-chart" aria-hidden="true"></i></span>Charts <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="chart_flotcharts.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Flot Charts</a></li>
+                    <li><a href="chart_c3.html"><i class="fa fa-angle-right" aria-hidden="true"></i> C3 Charts</a></li>
+                    <li><a href="chart_google.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Google Charts</a></li>
+                    <li><a href="chart_rickshawcharts.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Rickshaw Charts</a></li>
+                    <li><a href="chart_chartist.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Chartist</a></li>
+                    <li><a href="chart_easypiechart.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Easy Pie Chart</a></li>
+                    <li><a href="chart_jQuerysparklines.html"><i class="fa fa-angle-right" aria-hidden="true"></i> jQuery Sparklines</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><span class="icon color9"><i class="fa fa-th" aria-hidden="true"></i></span>Tables <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="basic-table.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Basic Tables</a></li>
+                    <li><a href="data-table.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Data Tables</a></li>
+                    <li><a href="jqgrid-table.html"><i class="fa fa-angle-right" aria-hidden="true"></i> JqGrid Table</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><span class="icon color10"><i class="fa fa-check-square-o" aria-hidden="true"></i></span>Forms <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="form-elements.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Form Elements</a></li>
+                    <li><a href="layouts.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Form Layouts</a></li>
+                    <li><a href="form-wizard.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Form Wizard</a></li>
+                    <li><a href="text-editors.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Text Editors</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><span class="icon color10"><i class="fa fa-globe" aria-hidden="true"></i></span>Miscellaneous <span class="label label-warning">New</span><i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="tour.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Tour</a></li>
+                    <li><a href="agile-board.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Agile board</a></li>
+                </ul>
+            </li>
+            <li><a href="maps.html"><span class="icon color7"><i class="fa fa-map-marker" aria-hidden="true"></i></span>Maps</a></li>
+            <li><a href="widgets.html"><span class="icon color11"><i class="fa fa-diamond" aria-hidden="true"></i></span>Widgets</a></li>
+            <li><a href="typography.html"><span class="icon color12"><i class="fa fa-font" aria-hidden="true"></i></span>Typography</a></li>
+            <li><a href="helper-classes.html"><span class="icon color8"><i class="fa fa-code" aria-hidden="true"></i></span>Helper Classes</a></li>
+            <li><a href="css_animation.html"><span class="icon color15"><i class="fa fa-magic" aria-hidden="true"></i></span>Css Animation</a></li>
+            <li><a href="grid.html"><span class="icon color15"><i class="fa fa-columns" aria-hidden="true"></i></span>Grid System</a></li>
+            <li class="sidetitle">PAGES</li>
+            <li><a href="#"><span class="icon color14"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>E-commerce<span class="label label-danger">New</span> <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="products-grid.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Products grid</a></li>
+                    <li><a href="product-list.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Product list</a></li>
+                    <li><a href="product-edit.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Product edit</a></li>
+                    <li><a href="product-detail.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Product detail</a></li>
+                    <li><a href="shoping-cart.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Shoping cart</a></li>
+                    <li><a href="orders.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Orders</a></li>
+                    <li><a href="credit-card-form.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Credit Card Form</a></li>
+                </ul>
+            </li>
+            <li><a href="mailbox.html"><span class="icon color6"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>Mailbox<span class="label label-default">3</span></a></li>
+            <li><a href="calendar.html"><span class="icon color8"><i class="fa fa-calendar-o" aria-hidden="true"></i></span>Calendar <span class="label label-danger">2</span></a></li>
+            <li><a href="#"><span class="icon color14"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></span>Extra Pages <i class="fa fa-sort-desc caret"></i></a>
+                <ul>
+                    <li><a href="social-profile.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Social Profile</a></li>
+                    <li><a href="invoice.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Invoice<span class="label label-danger">3</span></a></li>
+                    <li><a href="login.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Login Page</a></li>
+                    <li><a href="register.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Register</a></li>
+                    <li><a href="forgot-password.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Forgot Password</a></li>
+                    <li><a href="lockscreen.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Lockscreen</a></li>
+                    <li><a href="blank.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Blank Page</a></li>
+                    <li><a href="contact.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Contact</a></li>
+                    <li><a href="404.html"><i class="fa fa-angle-right" aria-hidden="true"></i> 404 Page</a></li>
+                    <li><a href="500.html"><i class="fa fa-angle-right" aria-hidden="true"></i> 500 Page</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<?php
+	include "inc/header.php"
+?>
+
+<?php
+    include "inc/sidebar.php"
+?>
+    
+
+<?php
+    include "inc/dashboard.php"
+?> 
+
+<?php
+	include "inc/rightbar.php"
+?>
+    
+    
+    
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap/tether.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins.js"></script>
+    <script type="text/javascript" src="assets/js/metismenu/metisMenu.min.js"></script>
+    <script type="text/javascript" src="assets/js/counterup/jquery.waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/counterup/jquery.counterup.min.js"></script>
+</body>
+
+</html>
